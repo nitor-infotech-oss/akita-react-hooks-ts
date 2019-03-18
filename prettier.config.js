f9f95b992494cf198ceec5b5bfdb04a9e58f3ac1
Sushant <sushantk@nitorinfotech.com>
2019-03-18 12:05:31 +0530

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all', // other options `es5` or `all`
+  bracketSpacing: true,
+  arrowParens: 'avoid', // other option 'always'
+};
